5efe6d49d3848412e109c7afb5a2421e800f3a0e
jasonmpittman <jmpittman@captechu.edu>
2019-04-05 17:09:23 -0400

    initial commit for twitter-bot program to pair with reddit-bot

---
+++ twitter-bot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created on 04/04/2019
+# @author: Jason M. Pittman
+# @license: MIT-license
+# Purpose: Read reddit posts from JSON and create tweets
+
+
+
+
+
+
+
+# THIS IS GOING TO MOVE TO ANOTHER SERVICE
+#twitter
+#auth = tweepy.OAuthHandler(config.TWITTER_API_KEY, config.TWITTER_API_SECRET)
+#auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_SECRET)
+#api = tweepy.API(auth)
+
+# build and post tweet
+#for submission in subreddit.hot(limit=3):
+#    if submission.title != 'Welcome to /r/artificial!':
+#        tweet = submission.title + "\n" + submission.url + "\n" "#AI #ArtificialIntelligence #drjmpBot"
+#        api.update_status(tweet) 
+#        time.sleep(5)
